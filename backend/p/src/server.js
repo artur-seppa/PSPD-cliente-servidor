3256db0a219d599c7207e53c93b91312e055e7b2
artur-seppa <artur.seppa@codeminer42.com>
2025-05-01 16:38:58 -0300

    feat: integração com frontend

---
+++ backend/p/src/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path');
+
+// Carregar o arquivo .proto
+const PROTO_PATH = path.join(__dirname, '../proto/service.proto');
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const proto = grpc.loadPackageDefinition(packageDefinition);
+
+// Configurar clientes gRPC
+const clientA = new proto.ServicoA('localhost:50051', grpc.credentials.createInsecure());
+const clientB = new proto.ServicoB('localhost:50052', grpc.credentials.createInsecure());
+
+// Configurar Express
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/usuario/:id', (req, res) => {
+  clientA.ConsultarUsuario({ id: req.params.id }, (err, resposta) => {
+    if (err) return res.status(500).json({ erro: err.details });
+    res.json(resposta);
+  });
+});
+
+app.post('/estatisticas', (req, res) => {
+  clientB.CalcularEstatisticas({ valores: req.body.valores }, (err, resposta) => {
+    if (err) return res.status(500).json({ erro: err.details });
+    res.json(resposta);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor P (Node.js) rodando em http://localhost:3000');
+});
